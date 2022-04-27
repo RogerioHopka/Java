1e79029368ce249763a9160a2bbe818a71c05879
Rogério Hopka <65628990+RogerioHopka@users.noreply.github.com>
2022-04-27 15:23:07 -0300

    Adicionei as minhas primeiras linhas de código em Java

---
+++ MeuPrimeiroProjetoJava/src/br/com/rogeriohopka/git/JavaPrimeiroProjeto.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package br.com.rogeriohopka.git;
+
+/**
+ * @author Rogério Hopka
+ * Começando com Java
+ *
+ */
+public class JavaPrimeiroProjeto {
+
+	
+	 public static void main(String[] args) {
+		System.out.println("Meu Primeiro Projeto Java!");
+
+	}
+
+}
